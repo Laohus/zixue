bfcc43a4b459232f05fc7ece1386720c780faef8
Laohus <1143253439@qq.com>
2020-11-18 10:13:18 +0800

    ceshi

--- src/main/java/com/xuexi/zixue/Controller/admin/loginpage.java
+++ src/main/java/com/xuexi/zixue/Controller/admin/loginpage.java
@@ -2,38 +2,33 @@ package com.xuexi.zixue.Controller.admin;
 
 import com.xuexi.zixue.Controller.checkdata.connsql;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @Controller
-@RequestMapping(method = {RequestMethod.GET,RequestMethod.POST})
 public class loginpage {
+
     @RequestMapping("/")
     public String login() {
-        return "/page/login.html";
-
+        return "login";
     }
 
     @PostMapping("/home")
     public String loginhome(@RequestParam("username") String username, @RequestParam("password") String password,
                             Map<String,Object> map){
-
         boolean res = new connsql().checklogin(username,password);
-        if (username==null || password==null){
-            map.put("msg","输入的用户信息不能为null");
-            return "redirect:/";
+        if (username.equals("null") || password.equals("null")){
+            map.put("messageerror","输入的用户信息不能为null");
+            return "login";
         }
         if (res){
             return "redirect:/page/home.html";
         }else {
-            map.put("msg","输入的用户信息不正确");
-            return "redirect:/";
+            map.put("messageerror","输入的用户信息不正确");
+            return "login";
 
         }
     }
--- src/main/java/com/xuexi/zixue/ZixueApplication.java
+++ src/main/java/com/xuexi/zixue/ZixueApplication.java
@@ -13,26 +13,4 @@ public class ZixueApplication {
         SpringApplication.run(ZixueApplication.class, args);
     }
 
-//    @RequestMapping("/")
-//    public String login(){
-//        return "/page/login.html";
-//
-//    }
-//
-//    @GetMapping ("/home")
-//    public String loginhome(@RequestParam("username") String username, @RequestParam("password") String password,
-//                            Model model){
-//        boolean res = new connsql().checklogin(username,password);
-//        if (res){
-//            return "/page/home.html";
-//        }else {
-////            map.put("msg","输入账号信息不正确，请重试！");
-//            model.addAttribute("msg","输入账号信息不正确，请重试！");
-//
-//            return "/";
-//
-//        }
-//
-//    }
-
 }
